672ff0b746756d1c665e4d75640dbd3298c8ffa7
Tmplt <tmplt@dragons.rocks>
2019-06-23 22:08:45 +0200

    feat(dulcia): stream MPD via Icecast

--- systems/dulcia.nix
+++ systems/dulcia.nix
@@ -1,6 +1,9 @@
+let
+  secrets = (import ../secrets).dulcia;
+in
 {
   dulcia = {
-    deployment.targetHost = "dulcia.localdomain";
+    deployment.targetHost = "den.dragons.rocks";
     time.timeZone = "Europe/Stockholm";
     networking.hostName = "dulcia";
 
@@ -41,12 +44,58 @@
       monthly = 1; # keep only one monthly snapshot
     };
 
+    services.mpd = {
+      enable = true;
+      user = "tmplt";
+      group = "users";
+      musicDirectory = "/media/music";
+      extraConfig = ''
+        password "${secrets.mpdPassword}@read,control,add,admin"
+        bind_to_address "192.168.0.101"
+        port "6600"
+
+        audio_output {
+          type "shout"
+          encoding "ogg"
+          name "local icecast stream"
+          host "localhost"
+          port "8000"
+          mount "/mpd.ogg"
+          password "${secrets.icecast.sourcePassword}"
+          quality "10.0"
+          format "44100:16:1"
+          description "find /media/music | xargs mpv --shuffle"
+          genre "madness"
+        }
+
+        audio_output {
+          type "null"
+          name "fake out"
+        }
+      '';
+    };
+
+    services.icecast = {
+      enable = true;
+      admin.password = secrets.icecast.adminPassword;
+      hostname = "den.dragons.rocks";
+      listen.port = 8000;
+      extraConf = ''
+        <authentication>
+          <source-password>${secrets.icecast.sourcePassword}</source-password>
+        </authentication>
+      '';
+    };
+
     # qemu/kvm
     users.groups.libvirtd.members = [ "root" "tmplt" ];
     virtualisation.libvirtd.enable = true;
 
     # Open ports in the firewall.
-    networking.firewall.allowedTCPPorts = [ 2049 ];
+    networking.firewall.allowedTCPPorts = [
+      2049 # ?
+      6600 8000 # MPD and Icecast
+    ];
     networking.firewall.allowedUDPPorts = [ 2049 ];
 
     # This value determines the NixOS release with which your system is to be
