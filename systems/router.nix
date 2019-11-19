7d12f52a85ab41d67ae553154b7999137a9099e0
Tmplt <tmplt@dragons.rocks>
2019-11-19 23:05:36 +0100

    fix(router/mpd): mult max_output_buffer_size by 16
    So that my whole library can be sent to remove clients.

--- systems/router.nix
+++ systems/router.nix
@@ -53,6 +53,7 @@ in
         password "${secrets.mpdPassword}@read,control,add,admin"
         bind_to_address "192.168.0.101"
         port "6600"
+        max_output_buffer_size "${toString (8192 * 16)}"
 
         audio_output {
           type "shout"
