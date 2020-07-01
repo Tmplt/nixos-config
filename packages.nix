8043b67e64ca283695d8fe089fe7bad861f3e7fa
tmplt <tmplt@dragons.rocks>
2020-07-01 11:59:23 +0200

    feat(pkgs): install an updated version of openocd
    openocd latest release was in 2017, which lacks some features I require.

--- packages.nix
+++ packages.nix
@@ -49,6 +49,33 @@ in {
 
       # I need v5.0
       zoom-us = unstable.zoom-us;
+
+      # Latest tagged release is from 2017, which lacks some scripts I need.
+      openocd = with stable; openocd.overrideAttrs (old: {
+        src = fetchgit {
+          url = "https://git.code.sf.net/p/openocd/code";
+          rev = "7c88e76a76588fa0e3ab645adfc46e8baff6a3e4";
+          sha256 = "0qli4zyqc8hvbpkhwscsfphk14sdaa1zxav4dqpvj21kgqxnbjr8";
+          fetchSubmodules = false; # repo.or.cz which hosts submodules is down
+        };
+
+        # no longer applies
+        patches = [];
+        postPatch = ''
+          ${gnused}/bin/sed -i "s/\''${libtoolize}/libtoolize/g" ./bootstrap
+          ${gnused}/bin/sed -i '7,14d' ./bootstrap
+        '';
+
+        buildInputs = old.buildInputs ++ [ automake autoconf m4 libtool tcl jimtcl ];
+
+        preConfigure = ''
+          ./bootstrap nosubmodule
+        '';
+        configureFlags = old.configureFlags ++ [
+          "--disable-internal-jimtcl"
+          "--disable-internal-libjaylink"
+        ];
+      });
     };
   };
 
