66bdbfc78c183a5f0c17e7eb2797b7d565f54081
Viktor Sonesten <v@tmplt.dev>
2020-12-28 05:46:06 +0100

    laptop: absorb common.nix

--- laptop.nix
+++ laptop.nix
@@ -3,6 +3,7 @@
   let
     secrets = (import ../secrets);
 
+    # TODO use a channel instead
     nixos-hardware = fetchTarball {
       sha256 = "07mp73a5gyppv0cd08l1wdr8m5phfmbphsn6v7w7x54fa8p2ci6y";
       url = "https://github.com/NixOS/nixos-hardware/archive/40ade7c0349d31e9f9722c7331de3c473f65dce0.tar.gz";
@@ -12,18 +13,100 @@
     deployment.targetHost = "localhost";
     networking.hostName = "perscitia";
 
+    time.timeZone = "Europe/Stockholm";
+    sound.enable = true;
+    boot.cleanTmpDir = true;
+
+    hardware = {
+      # Update Intel microcode on boot (both systems use Intel)
+      cpu.intel.updateMicrocode = true;
+
+      pulseaudio = {
+        enable = true;
+        support32Bit = true;
+
+        # Don't mute audio streams when VOIP programs are running.
+        extraConfig = ''
+          unload-module module-role-cork
+        '';
+      };
+
+      opengl = {
+        enable = true;            # required by sway
+        driSupport = true;
+        driSupport32Bit = true;
+      };
+    };
+
     networking = {
       interfaces = {
         enp0s25.useDHCP = true;
         wlp3s0.useDHCP = true;
       };
+
+      firewall.allowedUDPPorts = [ 7667 ]; # lcm
     };
 
     imports = [
-      ../hardware-configurations/laptop.nix
-      ../common.nix
-      ../wlan.nix
+      ./hardware-configurations/laptop.nix
+      ./wlan.nix
+      ./email.nix
+      ./tmplt.nix
+      ./packages.nix
     ];
+
+    # Make the default download directory a tmpfs, so I don't end up
+    # using it as a non-volatile dir for whatever.
+    #
+    fileSystems."/home/tmplt/tmp" = {
+      device = "tmpfs";
+      fsType = "tmpfs";
+      options = [ "rw" "size=20%" "uid=tmplt" ];
+    };
+
+    programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+
+      # `compinit` is called in the user configuration. Don't call it twice.
+      enableGlobalCompInit = false;
+    };
+
+    environment.etc = {
+      "nix/pins/cacert".source = pkgs.cacert;
+      "nix/pins/mu".source = pkgs.mu;
+    };
+
+    systemd.coredump.enable = true;
+    services.udisks2.enable = true;
+    services.dictd.enable = true;
+    services.dnsmasq.enable = true;
+
+    # Fix for USB redirection in virt-manager(1).
+    security.wrappers.spice-client-glib-usb-acl-helper.source = "${pkgs.spice_gtk}/bin/spice-client-glib-usb-acl-helper";
+    environment.systemPackages = with pkgs; [ spice_gtk ];
+
+    # Allow some USB devices to be accessed without root privelages.
+    services.udev.extraRules = with lib; let
+      toUdevRule = vid: pid: ''
+      SUBSYSTEM=="usb", ATTR{idVendor}=="${vid}", ATTR{idProduct}=="${pid}", TAG+="uaccess", RUN{builtin}+="uaccess" MODE:="0666"
+    '';
+      setWorldReadable = idPairs:
+        concatStrings (map (x: let l = splitString ":" x; in toUdevRule (head l) (last l)) idPairs);
+    in (setWorldReadable [
+      "0483:374b" "0483:3748" "0483:3752" # ST-LINK/V2.1 rev A/B/C+
+      "15ba:002a" # ATM-USB-TINY-H JTAG interface
+      "1366:1015" # SEGGER (JLink firmware)
+      "0403:6014" # FT232H
+    ]) +
+    # Shutdown system on low battery level to prevents fs corruption
+    ''
+      KERNEL=="BAT0" \
+      , SUBSYSTEM=="power_supply" \
+      , ATTR{status}=="Discharging" \
+      , ATTR{capacity}=="[0-5]" \
+      , RUN+="${pkgs.systemd}/bin/systemctl poweroff"
+    '';
 
     # Use the systemd-boot EFI boot loader.
     boot.loader.systemd-boot.enable = true;
@@ -55,7 +138,7 @@
 
     boot.initrd.luks.devices.root = {
       name = "root";
-      device = "/dev/disk/by-uuid/${lib.removeSuffix "\n" (builtins.readFile ../hardware-configurations/laptop-luks.uuid)}";
+      device = "/dev/disk/by-uuid/${lib.removeSuffix "\n" (builtins.readFile ./hardware-configurations/laptop-luks.uuid)}";
       preLVM = true;
       allowDiscards = true;
     };
@@ -133,34 +216,7 @@
       enable = true;
       powertop.enable = true;
     };
-
-    services.udev.extraRules =
-      # TODO: systemd-mount --umount --lazy?
-      # TODO: kill all processes accessing files under mount?
-      # /proc/*/fd | grep dulcia ...
-      # Perhaps it would be easier to do in Python? Then we can do things like timeout before SIGKILL, etc.
-      let undockScript = pkgs.writeShellScript "undock-script" ''
-        ${pkgs.inetutils}/bin/logger -t DOCKING "Detected condition: undocked"
-        ${pkgs.inetutils}/bin/logger -t DOCKING "TODO: kill pulseaudio, or rescan to find DAC again."
-        ${pkgs.inetutils}/bin/logger -t DOCKING "TODO: SIGTERM all procs using files under /home/tmplt/dulcia, timeout to SIGKILL"
-        ${pkgs.inetutils}/bin/logger -t DOCKING "TODO: systemd-mount --umount /home/tmplt/dulcia"
-      ''; in
-      let dockScript = pkgs.writeShellScript "dock-script" ''
-        ${pkgs.inetutils}/bin/logger -t DOCKING "Detected condition: docked"
-        ${pkgs.inetutils}/bin/logger -t DOCKING "TODO: systemctl start home-tmplt-dulcia.mount"
-      ''; in
-    # Shutdown system on low battery level to prevents fs corruption
-    ''
-      KERNEL=="BAT0" \
-      , SUBSYSTEM=="power_supply" \
-      , ATTR{status}=="Discharging" \
-      , ATTR{capacity}=="[0-5]" \
-      , RUN+="${pkgs.systemd}/bin/systemctl poweroff"
-    ''
-    # Automagically change monitor setup
-    + ''
-      SUBSYSTEM=="platform", ENV{EVENT}=="undock", RUN+="${undockScript}"
-      SUBSYSTEM=="platform", ENV{EVENT}=="dock", RUN+="${dockScript}"
-    '';
   };
+
+  system.stateVersion = "18.03";
 }
