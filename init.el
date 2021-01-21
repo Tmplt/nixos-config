af931c8fcb06e80dd7f06dfd4f3e9217d3569225
Viktor Sonesten <v@tmplt.dev>
2021-01-21 05:34:43 +0100

    emacs/email: warn if expected attachment(s) are missing

--- init.el
+++ init.el
@@ -384,6 +384,40 @@
   (setq mu4e-compose-format-flowed t
         fill-flowed-encode-column fill-column
         message-cite-reply-position 'below)
+  ;; Warn if expected attachments are missing
+  (defun mbork/message-attachment-present-p ()
+    "Return t if an attachment is found in the current message."
+    (save-excursion
+      (save-restriction
+        (widen)
+        (goto-char (point-min))
+        (when (search-forward "<#part" nil t) t))))
+  (defcustom tmplt/message-attachment-intent-re
+    (regexp-opt '("attach"
+                  "attached"
+                  "attachment"
+                  "bifogar"
+                  "bifogad"))
+    "A regex which - if found in the message, and if there is no
+attachment - should launch the no-attachment warning.")
+  (defcustom mbork/message-attachment-reminder
+    "Are you sure you want to send this message without any attachment? "
+    "The default question asked when trying to send a message
+containing `mbork/message-attachment-intent-re' without an
+actual attachment.")
+  (defun mbork/message-warn-if-no-attachments ()
+    "Ask the user if s?he wants to send the message even though
+there are no attachments."
+    (when (and (save-excursion
+	         (save-restriction
+		   (widen)
+		   (goto-char (point-min))
+		   (re-search-forward tmplt/message-attachment-intent-re nil t)))
+	       (not (mbork/message-attachment-present-p)))
+      (unless (y-or-n-p mbork/message-attachment-reminder)
+        (keyboard-quit))))
+  :hook
+  (message-send . (lambda nil (mbork/message-warn-if-no-attachments)))
   :bind
   ("C-x m" . 'mu4e)
   ;; TODO quit mu4e when done?
